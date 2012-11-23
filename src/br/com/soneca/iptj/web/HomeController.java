3c31248f80fda2309e494aa20d5148c1a6b4b284
soneca <devel.andrerocha@gmail.com>
2012-11-23 19:44:41 -0200

    aeee

---
+++ src/br/com/soneca/iptj/web/HomeController.java
@@ -0,0 +1,17 @@
+package br.com.soneca.iptj.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("index")
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+
+}
